6d816d0e8bec32765b3c4226986863a2ead668f4
Leonardo Moreno <lmorenogr@gmail.com>
2021-06-19 00:49:54 -0300

    Dogecoin Sentiment Analysis - DONE

--- NLP/dogecoin_sentimentAnalysisAPP/dogecoin_sentiment.py
+++ NLP/dogecoin_sentimentAnalysisAPP/dogecoin_sentiment.py
@@ -1,10 +1,13 @@
 from os import truncate
+from datetime import date, timedelta
+from nltk.util import pr
 import tweepy
 import textblob
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import re
+
 #Dogecoin Analysis header
 print('+_____________________________________________________________________________________+')
 print('|  _____                             _                              _           _     |')
@@ -45,23 +48,63 @@ print('                                   ▒▒▒▒▒▒▒▒▒▒▀▀')
 # 3. Access Token
 # 4. Access secret Token
 
-#Collects the parameters from keys.txt file
-twiterkeys = open('keys.txt' , 'r').read().splitlines()
-api_key =            twiterkeys [0]
-secret_key =         twiterkeys [1]
-access_token =       twiterkeys [2]
-accessSecret_token = twiterkeys [3]
+#Collects the parameters from a keys.txt file
+#Create your own file with own parameters. Always include they in the order specified above
+twitterkeys = open('keys.txt' , 'r').read().splitlines()
+api_key =            twitterkeys [0]
+secret_key =         twitterkeys [1]
+access_token =       twitterkeys [2]
+accessSecret_token = twitterkeys [3]
 print("\n>> Leitura das keys completa")
 print('Api:          ', api_key)
 print('Secret Key:   ', secret_key)
 print('Access Token: ', access_token)
 print('Secret Access Token: ', accessSecret_token)
-#no terminal funciona mas no vscode nao (????)
+print('_' * 70)
 
-aunthenticator = tweepy.OAuthHandler(api_key, secret_key)
-aunthenticator.set_access_token(access_token, accessSecret_token)
+auth = tweepy.OAuthHandler(api_key,secret_key)
+auth.set_access_token(access_token, accessSecret_token)
+api = tweepy.API(auth)
 
-api = tweepy.API(aunthenticator, wait_on_rate_limit=True)
 
-dogecoin = 'Dogecoin'
-search = f'#{dogecoin}'
+#Definition of dates
+today = date.today()
+weekago  = date.today() - timedelta(days=7) #subtracs 7 days (one week) from the present date
+
+doge = 'Dogecoin'
+search = f'#{doge} -filter:retweets' #defines the search term ('q' parameter at tweet_cursor)
+# filtes for retweets, maintaing the search minimal as possible and also focusing in the data that matters
+
+tweet_cursor = tweepy.Cursor(api.search, q=search, lang='en', until = today, since=weekago, tweet_mode='extended').items(100)
+# tweet_mode='extended' :  the text attribute of Status objects returned by tweepy.API methods is replaced
+# by a full_text attribute, which contains the entire untruncated text of the Tweet
+# items define the quantity of tweets to be analysed
+
+tweets = [tweet.full_text for tweet in tweet_cursor]
+#list comprehenssion that gets each tweet from tweet_cursor and saves it into a list
+
+#Converting the tweets into a dataframe utilizing the Pandas library
+tweets_df = pd.DataFrame(tweets, columns=['Tweets'])
+
+#Processing of cleaning the tweets for analysis
+for _, row in tweets_df.iterrows():
+    row['Tweets'] = re.sub('http\S+' , '', row['Tweets'])
+    row['Tweets'] = re.sub('#\S+'    , '', row['Tweets'])
+    row['Tweets'] = re.sub('@\S+'    , '', row['Tweets'])
+    row['Tweets'] = re.sub('\\n'     , '', row['Tweets'])
+#re.sub : Return the string obtained by replacing the leftmost nonNEGATIVEoverlapping occurrences of pattern
+#in string by the replacement repl. If the pattern isn’t found, string is returned unchanged.
+
+#Perform a sentiment analysis for all the individual Tweets and save a polarity score for them
+tweets_df['Polarity']  = tweets_df['Tweets'].map(lambda tweet: textblob.TextBlob(tweet).sentiment.polarity)
+tweets_df['Sentiment'] = tweets_df['Polarity'].map(lambda polarity: 'POSITIVE' if polarity > 0 else 'NEGATIVE')
+
+#Counts the quantity of positive vs. negative tweets
+positive = tweets_df[tweets_df.Sentiment == 'POSITIVE'].count()['Tweets']
+negative = tweets_df[tweets_df.Sentiment == 'NEGATIVE'].count()['Tweets']
+
+#Plot the graph utilizing matplotlib
+plt.bar([0, 1], [positive, negative], label = ['Positive', 'Negative'], color=['blue', 'red'])
+plt.title('Dogecoin Fear vs Greed this week\n' 
+          + str(weekago) + ' - ' + str(today))#subtitle
+plt.show()
