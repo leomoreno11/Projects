c4fd66682d1617cf607d24ae3cb3d97fcfddf7e6
Leonardo Moreno <lmorenogr@gmail.com>
2021-07-15 16:28:37 -0300

    Create iec.py
    Program that auxiliates calculation of investiments with solid percentage returns. Created for personal use

---
+++ Income expectation calculator/iec.py
@@ -0,0 +1,65 @@
+import numpy as np
+import pandas as pd
+
+amount = float(input('Whats the amount you want to invest ?\n'))
+rate = float(input('Whats the growth rate percentage of your investment ?\n'))
+months = int(input('For how many months ?\n'))
+contribution = float(input('There will be a monthly contribution?\n'))
+
+#testing parameters
+#amount  = 16340.25
+#rate    = 0.34
+#months  = 12
+#contribution  = 1233.18
+
+print('-'* 101 + '\nYou plan to invest ' + str(amount) + ' at a ' + str(rate) + '% per month, for ' + str(months) +  ' months and with a ' + str(contribution) + ' monthly contribution\n'
+         + '-'* 101)
+
+rate = rate / 100
+
+def investiment(amount, rate, months, contribution):
+    results     = []
+    profits     = []
+    contributions = []
+    profitsSum  = []
+    contributionSum = []
+    i = 0
+    j = 0
+    x = 0
+    c = []
+
+    while j < months:
+        contributions.append(contribution)
+        j+=1
+
+    while i < months:
+        profit = (amount * rate)
+        finalAmount = profit + amount
+        a = round(finalAmount, 2)
+        c.insert(i, 1)
+        monthSum = np.cumsum(c)
+        results.insert(i, a)
+        profits.insert(i, round(profit, 2))
+        # print(contributionSum)
+        contributionSum   = np.round(np.cumsum(contributions), decimals = 2)
+        profitsSum  = np.round(np.cumsum(profits), decimals = 2)
+        
+        amount = a + contribution
+        i += 1
+
+    # adicionar a opção de um contribution mensal
+
+    #while x < len(results):
+    #     #print('Month {} | $ {}'.format(c, results[x]) + ' | Profit of {}'.format(profits[x]))
+    #     #print('Month' + '\t\t' + 'Result' + '\t\t' + '  Profit' + '\t\t' + 'Total Profits')
+    #     #print('{}'.format(c) + '\t\t' + '{}'.format(results[x]) + '\t\t' + '  {}'.format(profits[x]) + '\t\t' + '{}'.format(profitsSum[x]))
+    #     #print('')
+    #     df = pd.DataFrame({'Month': monthSum, 'Result': results, 'Profit' : profits, 'Total Profits' : profitsSum})
+    #
+    #     x += 1
+    
+    df = pd.DataFrame({'Month': monthSum, 'Result': results, 'Profit' : profits, 'Total Profits' : profitsSum, 'Total Contributions' : contributionSum})
+    print(df.to_string(index=False))
+
+
+investiment(amount, rate, months, contribution)
